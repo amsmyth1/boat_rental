cf11791cfd180ddf9f2563619db9211b31f0b1d5
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-03 12:54:53 -0500

    Complete renter class and tests

--- lib/renter.rb
+++ lib/renter.rb
@@ -1,2 +1,9 @@
 class Renter
-end 
+  attr_reader :name,
+              :credit_card_number
+
+  def initialize(name, credit_card_number)
+    @name = name
+    @credit_card_number = credit_card_number
+  end 
+end
